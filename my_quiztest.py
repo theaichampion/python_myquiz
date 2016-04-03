8ab8fcac327c7b611b4f8d7612c749d9c9b2676b
Ben <superben9@Bens-MacBook-Pro.local>
2016-04-02 20:15:24 -0500

    second commit

--- my_quiztest.py
+++ my_quiztest.py
@@ -1,6 +1,18 @@
 print "Would you like to play a game?"
 answer = raw_input("type yes or no ")
 if answer == 'yes' or answer == 'y':
-    print  "Welcome to the introduction to your partner game. A quiz game where you will answer questions about your partner*******"
+    print  "Welcome to the fill in the blank game. Choose your level of diffculty, Easy, Medium and Hard"
+    answer1 = raw_input("type Easy, Medium or Hard ")
+    if answer1 == "Easy":
+        print "welcome to the Easy game" 
+        #easygame()
+    else:
+        if answer1 == "Medium":
+            print "welcome to the Medium game" 
+            #mediumegame()
+        else:
+            if answer1 == "Hard":
+                print "welcome to the Hard game"
+                #hardgame
 else:
     print "Maybe next time" 
