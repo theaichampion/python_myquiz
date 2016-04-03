d930adf1610661ec46a62356944be8e59663a3dc
Ben <superben9@Bens-MacBook-Pro.local>
2016-04-02 19:05:49 -0500

    I'm testing the git commit

---
+++ my_quiztest.py
@@ -0,0 +1,6 @@
+print "Would you like to play a game?"
+answer = raw_input("type yes or no ")
+if answer == 'yes' or answer == 'y':
+    print  "Welcome to the introduction to your partner game. A quiz game where you will answer questions about your partner*******"
+else:
+    print "Maybe next time" 
